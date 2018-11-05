d2323ab5b9fc02fe269807d5f232894904d21313
kizmo04 <get.kizmo@gmail.com>
2018-11-05 16:23:12 +0900

    complete 12_ap

--- source/12_ap.js
+++ source/12_ap.js
@@ -19,8 +19,34 @@
  * @symb R.ap([f, g], [a, b]) = [f(a), f(b), g(a), g(b)]
  *
  */
-var ap = function ap (applyF, applyX) {
+var ap = function ap (...args) {
+  let result = [];
+  switch (args.length) {
+    case 0:
+      return ap;
+    case 1:
+      return ap.bind(this, arguments[0]);
+    case 2:
+      if (typeof arguments[0] === 'function' && typeof arguments[1] === 'function') {
+        return ap.bind(this, arguments[0], arguments[1]);
 
+      } else if (Array.isArray(arguments[0]) && Array.isArray(arguments[1])) {
+        arguments[0].forEach(fn => {
+          arguments[1].forEach(item => {
+            result.push(fn(item));
+          });
+        });
+        return result;
+      }
+    default:
+      for (let i = 0; i < arguments[2].length; i++) {
+        result.push(arguments[0](arguments[2][i]));
+      }
+      for (i = 0; i < arguments[2].length; i++) {
+        result.push(arguments[1](arguments[2][i]));
+      }
+      return result;
+  }
 };
 
 export default ap;
